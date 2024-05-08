3f86f4ed376d2de3acaa95b3fd895b293db11ad2
Filipe Albuquerque Russo <russo.filipe@gmail.com>
2024-05-08 15:05:48 -0300

    Update index.js
    Added {history: true} back.

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ var InLoveComponent = Vue.extend({
   template: "#inlove-component-template"
 });
 
-var router = new VueRouter();
+var router = new VueRouter({history: true});
 
 router.map({
   '/': {
